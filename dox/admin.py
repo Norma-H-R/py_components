5d9e602af62956f8254d0a1105a8e0c79078beac
杜尔克斯大公爵 <45644429+Norma-H-R@users.noreply.github.com>
2025-02-16 02:03:43 +0800

    推倒重来

--- dox/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import DoxModel
-# Register your models here.
-admin.site.register(DoxModel)
--- userupload/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserUploadModel
-# Register your models here.
-
-admin.site.register(UserUploadModel)
-
